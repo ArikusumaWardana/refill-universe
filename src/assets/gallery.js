65b92baaf06446b620492f25aeccbbe88e9a4765
Arikusuma Wardana <87961962+ArikusumaWardana@users.noreply.github.com>
2024-06-07 09:33:34 +0800

    add more gallery

--- src/assets/gallery.js
+++ src/assets/gallery.js
@@ -10,6 +10,15 @@ import Image6 from '@/assets/gallery/image6.jpg'
 import Image7 from '@/assets/gallery/image7.jpg'
 import Image8 from '@/assets/gallery/image8.jpg'
 import Image9 from '@/assets/gallery/image9.jpg'
+import Image10 from '@/assets/gallery/image10.jpg'
+import Image11 from '@/assets/gallery/image11.jpg'
+import Image12 from '@/assets/gallery/image12.jpg'
+import Image13 from '@/assets/gallery/image13.jpg'
+import Image14 from '@/assets/gallery/image14.jpg'
+import Image15 from '@/assets/gallery/image15.jpg'
+import Image16 from '@/assets/gallery/image16.jpg'
+import Image17 from '@/assets/gallery/image17.jpg'
+import Image18 from '@/assets/gallery/image18.jpg'
 
 
 export const gallery = ref([
@@ -22,4 +31,13 @@ export const gallery = ref([
     {image: Image7},
     {image: Image8},
     {image: Image9},
+    {image: Image10},
+    {image: Image11},
+    {image: Image12},
+    {image: Image13},
+    {image: Image14},
+    {image: Image15},
+    {image: Image16},
+    {image: Image17},
+    {image: Image18},
 ])
