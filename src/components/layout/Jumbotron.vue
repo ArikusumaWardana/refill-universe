c511742d7165d3aadef07960400ed1acaba7aba8
Arikusuma Wardana <87961962+ArikusumaWardana@users.noreply.github.com>
2024-06-07 10:01:25 +0800

    update responsive for text welcome

--- src/components/layout/Jumbotron.vue
+++ src/components/layout/Jumbotron.vue
@@ -38,8 +38,15 @@ import RefillLogo from '@/assets/refill-logo/refill-logo.png'
 }
 
 .content h1 {
+    -webkit-text-stroke: 0.5px blue;
+    text-shadow: 3.5px 3.5px 0.5px #019733;
+}
+
+@media (min-width: 768px) {
+    .content h1 {
     -webkit-text-stroke: 1.5px blue;
     text-shadow: 3.5px 3.5px 0.5px #019733;
+}
 }
 
 .content h2 {
