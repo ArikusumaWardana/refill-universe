8d5310f82affa06bd5370781b2ccb29fcce5c2e7
Arikusuma Wardana <87961962+ArikusumaWardana@users.noreply.github.com>
2024-06-07 18:55:04 +0800

    update text

--- src/components/layout/AboutUs.vue
+++ src/components/layout/AboutUs.vue
@@ -14,7 +14,7 @@ import AboutUsImage from '@/assets/about-us/logo-about-us.png'
                 <h2 class="text-2xl md:text-3xl lg:text-4xl font-bold mb-4 uppercase drop-shadow-lg tracking-wide">About Us</h2>
                 <p class="text-gray-700 text-base md:text-lg lg:text-xl text-justify md:text-start">
                     <span class="font-bold">Refill Universe</span> is a community of students from the Morning Informatics 2 class who have a passion for exploring and 
-                    discovering food places that offer a filling culinary experience. places that offer a satisfying culinary experience. 
+                    discovering food places that offer a filling culinary experience. Places that offer a saxtisfying culinary experience. 
                     This community is not just about sharing recommendations, but also builds togetherness through exciting and rewarding 
                     culinary journeys. With a spirit of exploration and sense of togetherness, Refill Universe becomes a place for its members 
                     to exchange information and enjoy the moments of togetherness in every bite of food they find.
