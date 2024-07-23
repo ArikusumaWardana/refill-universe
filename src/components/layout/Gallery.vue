dbcba2e0d86072f43c4a8adabfd92d0f1119bd9a
arikusumawardana09 <arikusumawardana09@gmail.com>
2024-07-23 00:51:26 +0000

    fixing styling responsive gallery

--- src/components/layout/Gallery.vue
+++ src/components/layout/Gallery.vue
@@ -28,7 +28,7 @@ const loadLessImages = () => {
         <div class="container mx-auto">
             <h2 class="text-3xl lg:text-4xl font-bold mb-8 text-center uppercase tracking-wide drop-shadow-lg">Gallery</h2>
             <div class="grid grid-cols-1 md:grid-cols-3 gap-1">
-                <img v-for="(item, index) in displayedImages" :key="index" :src="item.image" alt="Gallery Image" class="w-full w-100 h-auto md:w-80 md:h-80 object-cover rounded-sm">
+                <img v-for="(item, index) in displayedImages" :key="index" :src="item.image" alt="Gallery Image" class="w-full w-100 h-auto md:h-80 object-cover rounded-sm">
             </div>
             <div class="btn-load flex justify-center mt-2">
                 <button @click="loadMoreImages" v-if="displayedImages.length < gallery.length" class="px-6 py-2 w-full text-white text-base font-semibold transition-all tracking-wide">
