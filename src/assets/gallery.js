53cab57ebef08995a96cef8391eb4613b0713d3f
arikusumawardana09 <arikusumawardana09@gmail.com>
2024-07-23 03:48:40 +0000

    update the gallery import images to dinamic import images

--- src/assets/gallery.js
+++ src/assets/gallery.js
@@ -1,43 +1,22 @@
-import { ref } from 'vue'
+// gallery.js
+const importAllImages = import.meta.glob('@/assets/gallery/*.{png,jpg,jpeg,svg}');
 
-// Gallery Images
-import Image1 from '@/assets/gallery/image1.jpg'
-import Image2 from '@/assets/gallery/image2.jpg'
-import Image3 from '@/assets/gallery/image3.jpg'
-import Image4 from '@/assets/gallery/image4.jpg'
-import Image5 from '@/assets/gallery/image5.jpg'
-import Image6 from '@/assets/gallery/image6.jpg'
-import Image7 from '@/assets/gallery/image7.jpg'
-import Image8 from '@/assets/gallery/image8.jpg'
-import Image9 from '@/assets/gallery/image9.jpg'
-import Image10 from '@/assets/gallery/image10.jpg'
-import Image11 from '@/assets/gallery/image11.jpg'
-import Image12 from '@/assets/gallery/image12.jpg'
-import Image13 from '@/assets/gallery/image13.jpg'
-import Image14 from '@/assets/gallery/image14.jpg'
-import Image15 from '@/assets/gallery/image15.jpg'
-import Image16 from '@/assets/gallery/image16.jpg'
-import Image17 from '@/assets/gallery/image17.jpg'
-import Image18 from '@/assets/gallery/image18.jpg'
+export const loadGallery = async () => {
+  const imageEntries = [];
 
+  for (const path in importAllImages) {
+    const module = await importAllImages[path]();
+    const fileName = path.split('/').pop().split('.').shift(); // Extract the file name without extension
+    imageEntries.push({ fileName, image: module.default });
+  }
 
-export const gallery = ref([
-    {image: Image1},
-    {image: Image2},
-    {image: Image3},
-    {image: Image4},
-    {image: Image5},
-    {image: Image6},
-    {image: Image7},
-    {image: Image8},
-    {image: Image9},
-    {image: Image10},
-    {image: Image11},
-    {image: Image12},
-    {image: Image13},
-    {image: Image14},
-    {image: Image15},
-    {image: Image16},
-    {image: Image17},
-    {image: Image18},
-])
+  // Urutkan berdasarkan nama file yang diekstrak
+  imageEntries.sort((a, b) => {
+    const numA = parseInt(a.fileName.replace('image', ''));
+    const numB = parseInt(b.fileName.replace('image', ''));
+    return numA - numB;
+  });
+
+  // Hanya kembalikan gambar
+  return imageEntries.map(entry => ({ image: entry.image }));
+};
